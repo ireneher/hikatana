4ae5c0d40a51019427e4306c9d193d9c17128c06
ireneher <theireneh@gmail.com>
2020-10-24 17:09:01 +0100

    dependencies in package

--- setupWin.sh
+++ setupWin.sh
@@ -1,3 +1,3 @@
 #export HIKATANA_ROOT=$PWD
 #export KATANA_RESOURCES=$KATANA_RESOURCES;"C:\Users\their\dev\hikatana\resources"
-export PYTHONPATH="C:\Users\their\dev\hikatana\python"
+export PYTHONPATH="C:\Users\their\dev\hikatana\python;C:\Users\their\dev\hikatana\lib\site-packages"
